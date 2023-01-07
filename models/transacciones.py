f13e6989c94e5fa8507ca142d1734fb7f067f543
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2023-01-07 20:16:16 +0100

    Funciona todo

--- models/transacciones.py
+++ models/transacciones.py
@@ -5,8 +5,18 @@ class Transacciones(models.Model):
     _rec_name = "nombre"
     _description = "transacciones"
 
-    nombre = fields.Char(string="nombre")
+    nombre = fields.Char(string="nombre", compute='_nombre')
     comprador = fields.Many2one('usuarios')
     vendedor = fields.Many2one('usuarios')
     portatil = fields.Many2one('portatiles')
     precio = fields.Integer(string='Precio')
+
+    @api.depends('comprador', 'vendedor', 'portatil')
+    def _nombre(self):
+        for record in self:
+            record.nombre = record.comprador + ' compro el portatil ' + record.portatil + ' de ' + record.vendedor
+
+    @api.onchange('vendedor')
+    def _onchange_vendedor(self):
+        if self.vendedor.vendedor == False:
+            return Warning
