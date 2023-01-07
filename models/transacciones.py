e3b5056f86fee95dd9517eb02dfd49658fb0e5b5
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2023-01-07 22:55:32 +0100

    Añadidas funciones varias

--- models/transacciones.py
+++ models/transacciones.py
@@ -6,17 +6,42 @@ class Transacciones(models.Model):
     _description = "transacciones"
 
     nombre = fields.Char(string="nombre", compute='_nombre')
-    comprador = fields.Many2one('usuarios')
-    vendedor = fields.Many2one('usuarios')
-    portatil = fields.Many2one('portatiles')
-    precio = fields.Integer(string='Precio')
+    comprador = fields.Many2one('usuarios', required=True)
+    vendedor = fields.Many2one('usuarios', required=True)
+    portatil = fields.Many2one('portatiles', required=True)
+    precio = fields.Integer(string='Precio', required=True)
 
     @api.depends('comprador', 'vendedor', 'portatil')
     def _nombre(self):
         for record in self:
-            record.nombre = record.comprador + ' compro el portatil ' + record.portatil + ' de ' + record.vendedor
+            record.nombre = str(record.comprador.nombre) + ' compro el portatil ' + str(record.portatil.nombre) + ' de ' + str(record.vendedor.nombre)
 
     @api.onchange('vendedor')
     def _onchange_vendedor(self):
         if self.vendedor.vendedor == False:
-            return Warning
+            return {
+                'warning': {
+                    'title': "Error",
+                    'message': "El usuario no es vendedor",
+                },
+            }
+    
+    @api.onchange('vendedor', 'comprador')
+    def _onchange_usuarios(self):
+        if self.vendedor.dni == self.comprador.dni:
+            return {
+                'warning': {
+                    'title': "Error",
+                    'message': "No puede comprar un portatil a si mismo",
+                },
+            }
+
+    @api.onchange('portatil', 'vendedor')
+    def _onchange_portatil(self):
+        if self.portatil.propietario != self.vendedor:
+            return {
+                'warning': {
+                    'title': "Error",
+                    'message': "El portatil no pertenece al vendedor",
+                },
+            }
